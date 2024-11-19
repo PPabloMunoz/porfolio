b7d7e0b3f2ec31f70487f5a8316bc9c0fc3555c8
Pablo Muñoz <ppablomunozp@gmail.com>
2024-11-19 15:24:37 +0100

    Initial commit

---
+++ .prettierrc.mjs
@@ -0,0 +1,17 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  singleQuote: true,
+  trailingComma: 'es5',
+  semi: false,
+  tabWidth: 2,
+  jsxSingleQuote: true,
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
