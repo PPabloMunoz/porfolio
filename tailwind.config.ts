fc0b1bc44e04e6a6e184a5c1e85c85614c5abb5d
Pablo Muñoz <ppablomunozp@gmail.com>
2024-11-19 17:20:19 +0100

    Add skills and projects sections

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,18 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default {
-  content: ['./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config
+	content: ["./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}"],
+	theme: {
+		extend: {
+			colors: {
+				background: "hsl(var(--background-color))",
+				primary: {
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: "hsl(var(--secondary))",
+				card: "hsl(var(--card))",
+			},
+		},
+	},
+	plugins: [],
+} satisfies Config;
