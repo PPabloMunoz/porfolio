1afc63337efdad035fba2f33ae5c0e65ef969930
Pablo Muñoz <pablomunoz@pablomunoz.dev>
2026-01-27 20:57:59 +0100

    config wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://pablomunoz.dev",
   output: "static",
+
   vite: {
     plugins: [tailwindcss()],
   },
-});
 
+  adapter: cloudflare(),
+});
